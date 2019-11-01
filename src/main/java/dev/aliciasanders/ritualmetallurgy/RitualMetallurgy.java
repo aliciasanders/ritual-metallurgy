f431952e09a1770179f581ccb1391beab666ed7e
Alicia Sanders <alicia@aliciasanders.dev>
2019-11-01 13:44:16 -0400

    Initial Mod setup

--- src/main/java/dev/aliciasanders/ritualmetallurgy/RitualMetallurgy.java
+++ src/main/java/dev/aliciasanders/ritualmetallurgy/RitualMetallurgy.java
@@ -1,5 +1,10 @@
 package dev.aliciasanders.ritualmetallurgy;
 
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("ritualmetallurgy")
 public class RitualMetallurgy {
+
+    public static final String MOD_ID = "ritualmetallurgy";
 
 }
